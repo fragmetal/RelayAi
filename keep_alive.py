2099bc61feb5d1d4f9b702ecc296b6ac76e52c32
fragmetal <fragmetal88@gmail.com>
2023-10-23 13:40:00 +0800

    #

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,38 @@
-from flask import Flask,render_template
+import os
+import logging
+from flask import Flask, render_template, make_response, request
 from threading import Thread
+
 app = Flask(__name__)
+
+# Set the Flask logger to log only messages at WARNING level or higher
+app.logger.setLevel(logging.WARNING)
+
+# Configure the Werkzeug logger to log only messages at WARNING level or higher
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.WARNING)
+
+
 @app.route('/')
 def index():
-    return "Alive"
+  return "Alive"
+
+
+@app.after_request
+def after_request(response):
+  if request.method == 'OPTIONS':
+    response = make_response()
+  return response
+
+
 def run():
-  app.run(host='0.0.0.0',port=8080)
-def keep_alive():  
-    t = Thread(target=run)
-    t.start()
+  app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
+
+
+if __name__ == "__main__":
+  keep_alive()
